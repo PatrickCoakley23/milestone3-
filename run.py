f33ea5de54236a1ab02f54d610c875c9efe7ab77
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-07-09 13:49:46 +0000

    connected flask to pymongo. Tested by sample 'for loop'. Also created enironment variable for access key

--- run.py
+++ run.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
 
+
+app.config["MONGO_DBNAME"] = 'milestone3'
+app.config["MONGO_URI"] = os.environ['MONGO_URI']
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
